74dac94429a987a868762e1ed1efc32b7c8d8d63
nic <119258820+n3kotheking@users.noreply.github.com>
2026-02-23 14:44:55 +0100

    completato main menu

--- src/main/java/levi/progettone/controller/MenuController.java
+++ src/main/java/levi/progettone/controller/MenuController.java
@@ -8,6 +8,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
@@ -17,11 +18,18 @@ public class MenuController {
     @FXML
     private Button start;
     @FXML
-    private Button options;
-    @FXML
     private Button exit;
     @FXML
-    private Label errors;
+    private Label author;
+    @FXML
+    private ImageView gameImage;
+    @FXML
+    private VBox menuScreen;
+
+    public void initialize(){
+        author.setText("by nic :P");
+        impostaBanner();
+    }
 
     @FXML
     public void goStart(ActionEvent event) {
@@ -42,19 +50,13 @@ public class MenuController {
     }
 
     @FXML
-    public void apriMenu(){
-        errors.setText("non ci sono opzioni, semplicemnete fa figo avere un bottone in più :)");
-    }
-
-    @FXML
     public void esci(ActionEvent event){
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.close();
     }
 
-
-
-
-
-
+    @FXML
+    private void impostaBanner(){
+        gameImage.setImage(new Image(getClass().getResource("/levi/progettone/imgs/banner.png").toExternalForm()));
+    }
 }
